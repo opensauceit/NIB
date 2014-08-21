39fc50e4f160e8f95407175695ccd84e77fd5779
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2014-08-21 21:26:45 +0100

    More validation for exercise input.

--- docker/run-exercise.sh
+++ docker/run-exercise.sh
@@ -9,6 +9,11 @@ learnyounode select ${WORKSHOP} > /dev/null
 
 read INPUT
 
+if [ -z "${INPUT}" ]; then
+  echo "Please supply an input for the exercise!"
+  exit 1
+fi
+
 echo ${INPUT} > test.js
 
 learnyounode verify test.js
